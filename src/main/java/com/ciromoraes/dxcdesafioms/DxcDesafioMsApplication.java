ac0509264bd828eb716b39c6ea6f22cd7038daa4
ciro germano <ciromrs@gmail.com>
2023-10-02 22:35:57 -0300

    Initial Commit

---
+++ src/main/java/com/ciromoraes/dxcdesafioms/DxcDesafioMsApplication.java
@@ -0,0 +1,13 @@
+package com.ciromoraes.dxcdesafioms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxcDesafioMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxcDesafioMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciromoraes/dxcdesafioms/DxcDesafioMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciromoraes.dxcdesafioms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxcDesafioMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
