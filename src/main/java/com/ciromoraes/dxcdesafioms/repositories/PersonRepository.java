099f254a2fa072cac60f976e5c28a8ef6dcb2131
ciro germano <ciromrs@gmail.com>
2023-10-04 19:49:56 -0300

    Add additional queries in PersonRepository
    Add native and jpql queries in PersonRepository

--- src/main/java/com/ciromoraes/dxcdesafioms/repositories/PersonRepository.java
+++ src/main/java/com/ciromoraes/dxcdesafioms/repositories/PersonRepository.java
@@ -2,6 +2,9 @@ package com.ciromoraes.dxcdesafioms.repositories;
 
 import com.ciromoraes.dxcdesafioms.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +13,11 @@ import java.util.Optional;
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     public Optional<Person> findByCpf(String cpf);
+
+    @Query(value = "SELECT p FROM Person p WHERE p.id = :personId")
+    public Optional<Person> findById(@Param("personId") Long id);
+
+    @Modifying
+    @Query(value = "DELETE FROM PERSON WHERE PERS_CD_PERSON = ?1", nativeQuery = true)
+    public void deleteById(Long id);
 }
